8b71c30ba4ad4d1ae143380c565cb66097585f64
emran <emranabdurahaman309@gmail.com>
2025-04-08 02:33:52 +0300

    BUBBLE

--- bubble.cpp
+++ bubble.cpp
@@ -1,1 +1,22 @@
-
+#include <iostream>
+ #include <vector>
+ #include <ctime>
+ void bubbleSort(std::vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+ }
+ int main() {
+    std::vector<int> arr = {64, 34, 25, 12, 22, 11, 90};
+    clock_t start = clock();
+    bubbleSort(arr);
+    clock_t end = clock();
+    double time_taken = double(end - start) / CLOCKS_PER_SEC;
+    std::cout << "Bubble Sort Time: " << time_taken << " seconds\n
+ ";
+}
